336f61f8548f72c8c94adcc9c72b1b0548523252
BryanhSantos <161526305+BryanhSantos@users.noreply.github.com>
2024-10-10 20:55:09 -0300

    Updated color.js

--- app/misc/color.js
+++ app/misc/color.js
@@ -1,10 +1,10 @@
 export default {
-  APP_BG: "#fff",
-  FONT: "#303d49",
-  FONT_MEDIUM: "#636363",
-  FONT_LIGHT: "#b6b8b9",
-  MODAL_BG: "rgba(0, 0, 0, 0.2)",
-  ACTIVE_BG: "#5252ad",
-  ACTIVE_FONT: "#fff",
-  MODAL_LIST: "rgba(204,204,204,0.3)",
+  APP_BG: "#191414", // Spotify background color
+  FONT: "#FFFFFF", // Spotify font color
+  FONT_MEDIUM: "#FFFFFF", // Spotify medium font color
+  FONT_LIGHT: "#1DB954", // Spotify light font color
+  MODAL_BG: "rgba(0, 0, 0, 0.8)", // Spotify modal background color
+  ACTIVE_BG: "#1DB954", // Spotify active background color
+  ACTIVE_FONT: "#FFFFFF", // Spotify active font color
+  MODAL_LIST: "rgba(40, 40, 40, 0.8)", // Spotify modal list color
 };
